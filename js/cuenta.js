af5940b6d764be90086bcf0c66dc02fbd5f5b392
n4nito <eloy.aspe2004@gmail.com>
2025-09-25 22:57:41 -0300

    avanze#3

--- js/cuenta.js
+++ js/cuenta.js
@@ -17,3 +17,18 @@ function guardarUsuarioNuevo(datos) {
     usuarios.push(datos);
     localStorage.setItem('usuarios', JSON.stringify(usuarios));
 }
+// Buscar usuario por usuario o email
+function buscarUsuario(login) {
+    let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+    return usuarios.find(u => u.usuario === login || u.email === login);
+}
+
+// Guardar carrito asociado al usuario
+function guardarCarritoUsuario(usuario, carrito) {
+    localStorage.setItem('carrito_' + usuario, JSON.stringify(carrito));
+}
+
+// Obtener carrito asociado al usuario
+function obtenerCarritoUsuario(usuario) {
+    return JSON.parse(localStorage.getItem('carrito_' + usuario)) || [];
+}
